fcbb93079a2f14c75d9381247249ff6289a1afbd
Amy Cao <amy.369@hotmail.com>
2025-03-22 13:53:27 -0700

    Add TCN and pseudocode for main

---
+++ main.py
@@ -0,0 +1,27 @@
+# Set sequence length of data to be observed for prediction
+SEQ_LEN = 10
+
+def MakePrediction():
+    # Get recent SEQ_LEN days of Financial Data 
+
+    # Get recent SEQ_LEN days of Sentiment Data
+
+    # Obtain Sentiment Scores
+
+    # Combine Sentiment Scores with Financial Data
+
+    # Predict Future Prices and Confidence Levels
+
+    pass
+
+def TrainTCN():
+    # Get recent SEQ_LEN days of Financial Data 
+
+    # Get recent SEQ_LEN days of Sentiment Data
+
+    # Obtain Sentiment Scores
+
+    # Combine Sentiment Scores with Financial Data
+    
+    # Train TCN
+    pass
